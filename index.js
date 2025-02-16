269a1d1d33a13d178e0948685f214e9b4a85929d
Willian Pero <wilhampero@hotmail.com>
2025-02-16 17:41:34 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 // Definir o nome e XP do herói
-const nomeHeroi = "Willian";
-const experiencia = 8500;
+var nomeHeroi = "Willian";
+var experiencia = 8500;
 
-let nivel;
+var nivel; // Variável para armazenar o nível do herói
 
 // Estrutura de decisão para classificar o nível do herói
 if (experiencia < 1000) {
-    nivel = "Ferro";
+    nivel = "Ferro"; // Menos de 1000 XP
 } else if (experiencia >= 1001 && experiencia <= 2000) {
-    nivel = "Bronze";
+    nivel = "Bronze"; // Entre 1001 e 2000 XP
 } else if (experiencia >= 2001 && experiencia <= 5000) {
-    nivel = "Prata";
+    nivel = "Prata"; // Entre 2001 e 5000 XP
 } else if (experiencia >= 5001 && experiencia <= 7000) {
-    nivel = "Ouro";
+    nivel = "Ouro"; // Entre 5001 e 7000 XP
 } else if (experiencia >= 7001 && experiencia <= 8000) {
-    nivel = "Platina";
+    nivel = "Platina"; // Entre 7001 e 8000 XP
 } else if (experiencia >= 8001 && experiencia <= 9000) {
-    nivel = "Ascendente";
+    nivel = "Ascendente"; // Entre 8001 e 9000 XP
 } else if (experiencia >= 9001 && experiencia <= 10000) {
-    nivel = "Imortal";
+    nivel = "Imortal"; // Entre 9001 e 10000 XP
 } else if (experiencia >= 10001) {
-    nivel = "Radiante";
+    nivel = "Radiante"; // 10001 ou mais XP
 }
 
 // Exibir a mensagem final
-console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
